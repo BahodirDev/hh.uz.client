f743ca690f56460acb290fa0c2503025668c8485
vohidjon7 <118883647+vohidjon7@users.noreply.github.com>
2023-07-29 15:36:01 +0500

    2-qismi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Head from './component/head/Head';
+import Produksiya from './component/produksiya/Produksiya';
 
 function App() {
   return (
     <div>
       <Head/>
+      <Produksiya/>
     </div>
   );
 }
